e174e32289c67df28d451fa2cbdbd46a294e6ed3
Technopriest90 <erokhin.ag@yandex.ru>
2017-10-10 12:13:41 +0300

    Анимировал модалку

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var modal = document.querySelector('.modal');
+var modalOverlay  = document.querySelector(".modal-overlay");
+var orderLink = document.querySelector(".product-item__order");
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -10,5 +13,25 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+orderLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  modal.classList.add("modal-show");
+  modalOverlay.classList.add("modal-overlay-show");
+});
+
+modalOverlay.addEventListener("click", function (event) {
+  event.preventDefault();
+  modal.classList.remove("modal-show");
+  modalOverlay.classList.remove("modal-overlay-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (feedback.classList.contains("modal-show")) {
+      feedback.classList.remove("modal-show");
+    }
   }
 });
